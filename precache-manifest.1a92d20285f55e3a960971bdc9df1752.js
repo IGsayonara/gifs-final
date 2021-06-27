14f3ec0946f1a01ba384a68c8dae67414053ac71
IGsayonara <">
2021-06-27 10:27:22 +0300

    Initial commit

---
+++ precache-manifest.1a92d20285f55e3a960971bdc9df1752.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4bfe8d23772d7963ac",
+    "url": "css/app.16522314.css"
+  },
+  {
+    "revision": "161995b08ebca9b93b5f",
+    "url": "css/chunk-vendors.512c3dfc.css"
+  },
+  {
+    "revision": "580be62f4cfc1336da8615ec3455a39f",
+    "url": "img/logo.580be62f.png"
+  },
+  {
+    "revision": "fa119db4a80367744d35698f509e2faf",
+    "url": "img/preloader.fa119db4.gif"
+  },
+  {
+    "revision": "6fc324077ec455b57f345d95766c8bf1",
+    "url": "index.html"
+  },
+  {
+    "revision": "3a4bfe8d23772d7963ac",
+    "url": "js/app.589e04b4.js"
+  },
+  {
+    "revision": "161995b08ebca9b93b5f",
+    "url": "js/chunk-vendors.41e1b6df.js"
+  },
+  {
+    "revision": "89ae735e2a7906b2c077c7b673e42504",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
